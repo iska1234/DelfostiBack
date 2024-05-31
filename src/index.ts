1d0b1bc5ae913725fd710badb5089dd85af20104
iska1234 <sadhuisaac1@gmail.com>
2024-05-30 21:23:33 -0500

    Authentication methods implemented (Login, Register, Logout)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import sessionHandler from "./middlewares/session";
+import errorHandler from "./middlewares/error";
+import authRouter from "./routers/authRouter";
 
 const morgan = require("morgan");
 const app = express();
@@ -10,14 +13,15 @@ configDotenv();
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(sessionHandler());
 app.use(cors());
+app.use(errorHandler);
 app.use(
     morgan(":method :url :status :res[content-length] - :response-time ms")
   );
-  
 
-
+    
+app.use("/auth", authRouter);
 app.listen(port, () => console.log(`Escuchando al puerto ${port}`));
 
 export default app;
